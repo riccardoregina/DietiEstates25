ea028ad1b076fc034f506a8bcd98b5067edbae33
riccardoregina <riccardoregina04@gmail.com>
2025-02-11 14:41:26 +0100

    handle InvalidTokenException (created in this commit) in JwtAuthFilter: exceptions that are thrown before reaching the controller do not reach GlobalExceptionHandler.

--- src/main/java/it/unina/dietiestates25/auth/infrastructure/config/JwtAuthFilter.java
+++ src/main/java/it/unina/dietiestates25/auth/infrastructure/config/JwtAuthFilter.java
@@ -1,10 +1,13 @@
 package it.unina.dietiestates25.auth.infrastructure.config;
 
 import it.unina.dietiestates25.auth.infrastructure.util.JwtUtil;
+import it.unina.dietiestates25.exception.ErrorDetails;
+import it.unina.dietiestates25.exception.InvalidTokenException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -30,33 +33,44 @@ public class JwtAuthFilter extends OncePerRequestFilter {
   @Override
   protected void doFilterInternal(HttpServletRequest request,  HttpServletResponse response,  FilterChain filterChain)
       throws ServletException, IOException {
-    String authHeader = request.getHeader("Authorization");
-    String token = null;
-    String username = null;
+    try {
+      String authHeader = request.getHeader("Authorization");
+      String token = null;
+      String username = null;
 //  Get, if present, the JWT and the contained username
-    if (authHeader != null && authHeader.startsWith("Bearer ")) {
-      token = authHeader.substring(7);
-      username = JwtUtil.extractUsername(token);
-    }
+      if (authHeader != null && authHeader.startsWith("Bearer ")) {
+        token = authHeader.substring(7);
+        username = JwtUtil.extractUsername(token);
+      }
 
 //  If the accessToken is null. It will pass the request to next filter in the chain.
 //  Any login and signup requests will not have jwt token in their header, therefore they will be passed to next filter chain.
-    if (token == null) {
-      filterChain.doFilter(request, response);
-      return;
-    }
+      if (token == null) {
+        filterChain.doFilter(request, response);
+        return;
+      }
 
 //  If any accessToken is present, then it will check if the token is valid and then authenticate the request in security context
-    if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-      UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-      if (Boolean.TRUE.equals(JwtUtil.isTokenValid(token, userDetails))) {
-        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-        authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+      if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+        if (Boolean.TRUE.equals(JwtUtil.isTokenValid(token, userDetails))) {
+          UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+          authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+          SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        }
       }
-    }
 
-    filterChain.doFilter(request, response);
+      filterChain.doFilter(request, response);
+    } catch (InvalidTokenException e) {
+      response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+      response.setContentType("application/json");
+      var errorDetails = new ErrorDetails(
+              HttpStatus.UNAUTHORIZED.value(),
+              HttpStatus.UNAUTHORIZED.name(),
+              e.getMessage(),
+              request.getRequestURI());
+      response.getWriter().write(errorDetails.toJson());
+    }
   }
 
 }
