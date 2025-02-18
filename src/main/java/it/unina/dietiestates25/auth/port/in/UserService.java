ca15a45d7132993eb8d08d2bce7ed88ab8791a78
riccardoregina <riccardoregina04@gmail.com>
2025-02-18 11:18:29 +0100

    merge UserDetailsServiceImpl and UserService into UserService

--- src/main/java/it/unina/dietiestates25/auth/port/in/UserService.java
+++ src/main/java/it/unina/dietiestates25/auth/port/in/UserService.java
@@ -1,20 +1,40 @@
 package it.unina.dietiestates25.auth.port.in;
 
-import it.unina.dietiestates25.auth.port.out.UserRepository;
 import it.unina.dietiestates25.exception.EntityNotExistsException;
 import it.unina.dietiestates25.model.User;
+import it.unina.dietiestates25.auth.port.out.UserRepository;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService {
-    private final UserRepository<User> userRepository;
+public class UserService implements UserDetailsService {
 
-    public UserService(UserRepository<User> userRepository) {
-        this.userRepository = userRepository;
-    }
+  @Qualifier("userRepository")
+  private final UserRepository<User> repository;
 
-    public User getUser(String email) throws EntityNotExistsException {
-        return userRepository.findUserByEmail(email)
-                .orElseThrow(() -> new EntityNotExistsException(String.format("User does not exist, email: %s", email)));
-    }
+  public UserService(UserRepository<User> repository) {
+    this.repository = repository;
+  }
+
+  @Override
+  public UserDetails loadUserByUsername(String email)
+          throws UsernameNotFoundException {
+
+    User user = repository.findUserByEmail(email).orElseThrow(() ->
+        new UsernameNotFoundException("User not found"));
+
+    return org.springframework.security.core.userdetails.User.builder()
+        .username(user.getEmail())
+        .password(user.getPasswordHash())
+        .roles(user.getClass().getSimpleName().toUpperCase())
+        .build();
+  }
+
+  public User getUser(String email) throws EntityNotExistsException {
+    return repository.findUserByEmail(email)
+            .orElseThrow(() -> new EntityNotExistsException(String.format("User does not exist, email: %s", email)));
+  }
 }
