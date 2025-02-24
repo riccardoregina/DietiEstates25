c1732de6836faea3ae6d4220e5e6b01a3eaea028
francescorossetti03 <francesco.rossettiiii@gmail.com>
2025-02-24 16:22:07 +0100

    Add starred listings endpoint

---
+++ src/main/java/it/unina/dietiestates25/starredlisting/infrastructure/adapter/in/RestStarredListingController.java
@@ -0,0 +1,48 @@
+package it.unina.dietiestates25.starredlisting.infrastructure.adapter.in;
+
+import it.unina.dietiestates25.exception.EntityNotExistsException;
+import it.unina.dietiestates25.exception.ForbiddenException;
+import it.unina.dietiestates25.model.Listing;
+import it.unina.dietiestates25.starredlisting.port.in.StarredListingService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/starred-listings")
+public class RestStarredListingController {
+
+    private final StarredListingService starredListingService;
+
+    public RestStarredListingController(StarredListingService starredListingService) {
+        this.starredListingService = starredListingService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> addStarredListing(@RequestBody String listingId,
+                                                  @AuthenticationPrincipal UserDetails userDetails)
+            throws EntityNotExistsException {
+        starredListingService.addStarredListing(userDetails.getUsername(), listingId);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> removeStarredListing(@RequestParam String userId,
+                                                     @RequestParam String listingId,
+                                                     @AuthenticationPrincipal UserDetails userDetails)
+            throws ForbiddenException, EntityNotExistsException {
+        starredListingService.removeStarredListing(userDetails.getUsername(), listingId, userId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Listing>> getStarredListings(@RequestParam String userId,
+                                                            @AuthenticationPrincipal UserDetails userDetails)
+            throws ForbiddenException, EntityNotExistsException {
+        List<Listing> starredListings = starredListingService.getStarredListings(userId, userDetails.getUsername());
+        return ResponseEntity.ok().body(starredListings);
+    }
+}
