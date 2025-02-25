5f97d82ad0a3c5d6523c448cb991ed737fcf639e
riccardoregina <riccardoregina04@gmail.com>
2025-02-25 10:33:36 +0100

    fix allowed origins

--- src/main/java/it/unina/dietiestates25/auth/infrastructure/config/SecurityConfig.java
+++ src/main/java/it/unina/dietiestates25/auth/infrastructure/config/SecurityConfig.java
@@ -83,7 +83,7 @@ public class SecurityConfig {
   @Bean
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(List.of("*"));
+    configuration.setAllowedOrigins(List.of("http://localhost:4200")); // Your Angular app
     configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
     configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
     configuration.setAllowCredentials(true);
