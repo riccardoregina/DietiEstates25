1e143e2faaa9a717b5d2e6c0adebc8cd1e066485
riccardoregina <riccardoregina04@gmail.com>
2025-03-07 12:02:19 +0100

    add get and put endpoint to RestCustomerController

--- src/main/java/it/unina/dietiestates25/customer/infrastructure/adapter/in/RestCustomerController.java
+++ src/main/java/it/unina/dietiestates25/customer/infrastructure/adapter/in/RestCustomerController.java
@@ -3,9 +3,14 @@ package it.unina.dietiestates25.customer.infrastructure.adapter.in;
 import it.unina.dietiestates25.agency.infrastructure.adapter.in.dto.UserDto;
 import it.unina.dietiestates25.customer.port.in.CustomerService;
 import it.unina.dietiestates25.exception.EntityAlreadyExistsException;
+import it.unina.dietiestates25.exception.EntityNotExistsException;
+import it.unina.dietiestates25.exception.ForbiddenException;
 import it.unina.dietiestates25.model.Customer;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,6 +21,7 @@ import java.net.URI;
 public class RestCustomerController {
     private final CustomerService customerService;
     private final PasswordEncoder passwordEncoder;
+    public static final String FORBIDDEN_EXCEPTION_MSG_CUSTOMER = "A customer can only modify itself";
 
     public RestCustomerController(CustomerService customerService,
                                   PasswordEncoder passwordEncoder) {
@@ -34,5 +40,34 @@ public class RestCustomerController {
                 passwordEncoder.encode(userDto.password()),
                 userDto.profilePicUrl()));
         return ResponseEntity.created(URI.create("/api/customers/" + customer.getId())).body(customer);
+    }
+
+    @PutMapping("/{customer-id}")
+    @PreAuthorize("hasRole('CUSTOMER')")
+    public ResponseEntity<Void> updateCustomer(@PathVariable("customer-id") String customerId,
+                                              @Valid @RequestBody UserDto userDto,
+                                              @AuthenticationPrincipal UserDetails userDetails)
+            throws EntityNotExistsException, ForbiddenException {
+        Customer customer = customerService.getCustomer(userDetails.getUsername());
+        validateCustomer(customerId, customer, FORBIDDEN_EXCEPTION_MSG_CUSTOMER);
+        customerService.updateCustomer(userDto, customer);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{customer-id}")
+    @PreAuthorize("hasRole('CUSTOMER')")
+    public ResponseEntity<Customer> getCustomer(@PathVariable("customer-id") String customerId,
+                                                @AuthenticationPrincipal UserDetails userDetails)
+            throws EntityNotExistsException, ForbiddenException {
+        Customer customer = customerService.getCustomer(userDetails.getUsername());
+        validateCustomer(customerId, customer, FORBIDDEN_EXCEPTION_MSG_CUSTOMER);
+        return ResponseEntity.ok(customer);
+    }
+
+    private void validateCustomer(String customerId, Customer customer, String errorMsg)
+            throws ForbiddenException {
+        if (!customerId.equals(customer.getId())) {
+            throw new ForbiddenException(errorMsg);
+        }
     }
 }
--- src/main/java/it/unina/dietiestates25/customer/port/in/CustomerService.java
+++ src/main/java/it/unina/dietiestates25/customer/port/in/CustomerService.java
@@ -1,10 +1,13 @@
 package it.unina.dietiestates25.customer.port.in;
 
+import it.unina.dietiestates25.agency.infrastructure.adapter.in.dto.UserDto;
 import it.unina.dietiestates25.customer.port.out.CustomerRepository;
 import it.unina.dietiestates25.exception.EntityAlreadyExistsException;
 import it.unina.dietiestates25.exception.EntityNotExistsException;
 import it.unina.dietiestates25.model.Customer;
 import it.unina.dietiestates25.model.User;
+import jakarta.transaction.Transactional;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -12,9 +15,11 @@ import java.util.Optional;
 @Service
 public class CustomerService {
     private final CustomerRepository repository;
+    private final PasswordEncoder passwordEncoder;
 
-    public CustomerService(CustomerRepository repository) {
+    public CustomerService(CustomerRepository repository, PasswordEncoder passwordEncoder) {
         this.repository = repository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public Customer signUp(Customer customer)
@@ -31,5 +36,15 @@ public class CustomerService {
         return repository
                 .findByEmail(email).orElseThrow(() ->
                         new EntityNotExistsException(String.format("Customer with email '%s' not found", email)));
+    }
+
+    @Transactional
+    public void updateCustomer(UserDto userDto, Customer customer) {
+        customer.setEmail(userDto.email());
+        customer.setFirstName(userDto.firstName());
+        customer.setLastName(userDto.lastName());
+        customer.setDob(userDto.dob());
+        customer.setPasswordHash(passwordEncoder.encode(userDto.password()));
+        if (userDto.profilePicUrl() != null) customer.setProfilePicUrl(userDto.profilePicUrl());
     }
 }
