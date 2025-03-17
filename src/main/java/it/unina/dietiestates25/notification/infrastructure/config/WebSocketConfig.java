ba470a2c4a6a40fafb01265870c8c49863b7a995
riccardoregina <riccardoregina04@gmail.com>
2025-03-17 11:17:47 +0100

    add angular port to allowed origins

--- src/main/java/it/unina/dietiestates25/notification/infrastructure/config/WebSocketConfig.java
+++ src/main/java/it/unina/dietiestates25/notification/infrastructure/config/WebSocketConfig.java
@@ -21,7 +21,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:8080")
+                .setAllowedOrigins("http://localhost:4200", "http://localhost:8080")
                 .withSockJS();
     }
 
